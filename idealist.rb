5025db37e876f82545cb5af30058a2e532569385
Mark Phelps <mark.aaron.phelps@gmail.com>
2011-09-08 23:55:28 -0400

    Added JSON output for index/show

--- idealist.rb
+++ idealist.rb
@@ -3,7 +3,11 @@ require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'dm-migrations'
+require 'json'
+require 'haml'
+require 'sinatra/respond_to'
 
+Sinatra::Application.register Sinatra::RespondTo
 use Rack::MethodOverride
 
 DataMapper.setup(:default, {
@@ -18,20 +22,30 @@ DataMapper.setup(:default, {
 class Thought
   include DataMapper::Resource
 
-  property :id,         Serial    # primary serial key
-  property :body,       Text
+  property :id, Serial    # primary serial key
+  property :body, Text
   property :created_at, DateTime
   property :updated_at, DateTime
 
   validates_length_of :body, :minimum => 1
+
+  def to_json(*a)
+      {:id => id, :body => body, :created_at => created_at, :updated_at => updated_at}.to_json(*a)
+  end
 end
 
 DataMapper.auto_upgrade!
 
 get '/' do
   @thoughts = Thought.all(:order => [ :id.desc ])
-  if @thoughts
-    haml :index
+  haml :index
+end
+
+get '/thoughts' do
+  @thoughts = Thought.all(:order => [ :id.desc ])
+  respond_to do |wants|
+    wants.html { haml :index }
+    wants.json { @thoughts.to_ary.to_json }
   end
 end
 
@@ -48,7 +62,10 @@ end
 get '/:id' do
   @thought = Thought.get(params[:id])
   if @thought
-    haml :show
+    respond_to do |wants|
+      wants.html { haml :show }
+      wants.json { @thought.to_json }
+    end
   else
     redirect '/'
   end
