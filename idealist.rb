7ccdb7d1d22a1420d4e2e942c4d4fbecda41f31d
Mark Phelps <mark.aaron.phelps@gmail.com>
2011-11-27 23:17:33 -0500

    Removed notion of lists, went back to just ideas; added get single Idea api endpoint

--- idealist.rb
+++ idealist.rb
@@ -17,7 +17,7 @@ DataMapper.setup(:default, {
   :password => 'root'
 })
 
-class Thought
+class Idea
   include DataMapper::Resource
 
   property :id, Serial
@@ -27,27 +27,8 @@ class Thought
 
   validates_length_of :body, :minimum => 1
 
-  belongs_to :list, :required => true
-
   def to_json(*a)
     JSON.pretty_generate({:id => id, :body => body, :created_at => created_at, :updated_at => updated_at})
-  end
-end
-
-class List
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :name, String
-  property :created_at, DateTime
-  property :updated_at, DateTime
-
-  validates_length_of :name, :minimum => 1
-
-  has n, :thoughts
-
-  def to_json(*a)
-    JSON.pretty_generate({:id => id, :name => name, :created_at => created_at, :updated_at => updated_at})
   end
 end
 
@@ -56,223 +37,112 @@ DataMapper.auto_upgrade!
 
 ## API ##
 
-# Get all Lists
-get '/api/lists/?', :provides => 'json' do
-  lists = List.all(:order => [:id.desc])
-  lists.to_ary.to_json
+# Get all Ideas
+get '/api/ideas/?', :provides => 'json' do
+  ideas = Idea.all(:order => [:id.desc])
+  status 200
+  ideas.to_ary.to_json
 end
 
-# Create a List
-post '/api/lists/?' do
+# Get a single Idea
+get '/api/ideas/:id/?', :provides => 'json' do
+  idea = Idea.get(params['id'])
+  if !idea
+    status 404
+  else
+    status 200
+    idea.to_json
+  end
+end
+
+# Create an Idea
+post '/api/ideas/?' do
   data = JSON.parse(request.body.read.to_s)
-  if data.nil? || !data.has_key?('name')
+  if data.nil? || !data.has_key?('body')
     status 400
   else
-    list = List.new(:name => data['name'])
-    list.created_at = Time.now
-    list.updated_at = Time.now
-    if list.save
+    idea = Idea.new(:body => data['body'])
+    idea.created_at = Time.now
+    idea.updated_at = Time.now
+    if idea.save
       status 201
-      list.to_json
+      idea.to_json
     else
       status 412
     end
   end
 end
 
-# Update a List
-put '/api/lists/:id/?' do
-  list = List.get(params['id'])
-  if list
-    data = JSON.parse(request.body.read.to_s)
-    if data.nil? || !data.has_key?('name')
-      status 400
-    else
-      list.name = data['name']
-      list.updated_at = Time.now
-      if list.save
-        status 200
-        list.to_json
-      else
-        status 412
-      end
-    end
-  else
+# Update an Idea
+put '/api/ideas/:id/?' do
+  idea = Idea.get(params['id'])
+  if !idea
     status 404
-  end
-end
-
-# Delete a List
-delete '/api/lists/:id/?' do
-  list = List.get(params['id'])
-  if list
-    list.destroy
-    status 200
   else
-    status 404
-  end
-end
-
-# Get all Thoughts on a List
-get '/api/lists/:listId/thoughts/?' do
-  list = List.get(params[:listId])
-  if list
-    Thought.all(:list_id => list.id, :order => [:id.desc]).to_ary.to_json
-  else
-    status 404
-  end
-end
-
-# Create a Thought and add to an existing List
-post '/api/lists/:listId/thoughts/?' do
-  list = List.get(params[:listId])
-  if list
     data = JSON.parse(request.body.read.to_s)
     if data.nil? || !data.has_key?('body')
       status 400
     else
-      thought = Thought.new(:list_id => list.id, :body => data['body'])
-      thought.created_at = Time.now
-      thought.updated_at = Time.now
-      if thought.save
-        status 201
-        thought.to_json
-      else
-        status 412
-      end
-    end
-  else
-    status 404
-  end
-end
-
-# Update a Thought on an existing List
-put '/api/lists/:listId/thoughts/:id/?' do
-  list = List.get(params[:listId])
-  thought = Thought.get(params[:id])
-  if list && thought && thought.list.id === list.id
-    data = JSON.parse(request.body.read.to_s)
-    if data.nil? || !data.has_key?('body')
-      status 400
-    else
-      thought.body = data['body']
-      thought.updated_at = Time.now
-      if thought.save
+      idea.body = data['body']
+      idea.updated_at = Time.now
+      if idea.save
         status 200
-        thought.to_json
+        idea.to_json
       else
         status 412
       end
     end
-  else
-    status 404
   end
 end
 
-# Delete a Thought on an existing list
-delete '/api/lists/:listId/thoughts/:id/?' do
-  list = List.get(params[:listId])
-  thought = Thought.get(params[:id])
-  if list && thought && thought.list.id === list.id
-    thought.destroy
-    status 200
-  else
+# Delete an Idea
+delete '/api/ideas/:id/?' do
+  idea = Idea.get(params['id'])
+  if !idea
     status 404
+  else
+    idea.destroy
+    status 200
   end
 end
 
 ## HTML ##
 
-get '/lists/?', :provides => 'html' do
-  @lists = List.all(:order => [:id.desc])
-  haml :"lists/index"
+get '/ideas/?', :provides => 'html' do
+  @ideas = Idea.all(:order => [:id.desc])
+  haml :"ideas/index"
 end
 
-get '/lists/new/?' do
-  haml :"lists/new"
+get '/ideas/new/?' do
+  haml :"ideas/new"
 end
 
-post '/lists/create/?' do
-  @list = List.new(:name => params[:list_name])
-  if @list.save
-    redirect '/lists/'
+post '/ideas/create/?' do
+  @idea = Idea.new(:body => params[:idea_body])
+  if @idea.save
+    redirect '/ideas/'
   else
-    redirect '/lists/new/'
+    redirect '/ideas/new/'
   end
 end
 
-get '/lists/edit/:id/?', :provides => 'html'  do
-  @list = List.get(params[:id])
-  haml :"lists/edit"
+get '/ideas/edit/:id/?', :provides => 'html'  do
+  @idea = Idea.get(params[:id])
+  haml :"ideas/edit"
 end
 
-post '/lists/update/:id/?' do
-  @list = List.get(params[:id])
-  @list.name = params[:list_name]
-  if @list.save
-    redirect '/lists/'
+post '/ideas/update/:id/?' do
+  @idea = Idea.get(params[:id])
+  @idea.body = params[:idea_body]
+  if @idea.save
+    redirect '/ideas/'
   else
-    redirect "/lists/edit/#{@list.id}/"
+    redirect "/ideas/edit/#{@idea.id}/"
   end
 end
 
-delete '/lists/:id/?' do
-  @list = List.get(params[:id])
-  @list.destroy
-  redirect '/lists/'
-end
-
-get '/lists/:listId/thoughts/?', :provides => 'html'  do
-  @list = List.get(params[:listId])
-  @thoughts = Thought.all(:list_id => @list.id, :order => [:id.desc])
-  haml :"thoughts/index"
-end
-
-get '/lists/:listId/thoughts/new/?', :provides => 'html' do
-  @list = List.get(params[:listId])
-  haml :"thoughts/new"
-end
-
-post '/lists/:listId/thoughts/create' do
-  @list = List.get(params[:listId])
-  @thought = Thought.new(:list_id => params[:listId], :body => params[:thought_body])
-  if @thought.save
-    redirect "/lists/#{@list.id}/thoughts/"
-  else
-    redirect "/lists/#{@list.id}/thoughts/new/"
-  end
-end
-
-get '/lists/:listId/thoughts/edit/:id/?', :provides => 'html' do
-  @list = List.get(params[:listId])
-  @thought = Thought.get(params[:id])
-  if @thought.list.id === @list.id
-    haml :"thoughts/edit"
-  else
-    redirect "/lists/#{@list.id}/thoughts/"
-  end
-end
-
-post '/lists/:listId/thoughts/update/:id/?' do
-  @list = List.get(params[:listId])
-  @thought = Thought.get(params[:id])
-  if @thought.list.id === @list.id
-    @thought.body = params[:thought_body]
-    if @thought.save
-      redirect "/lists/#{@list.id}/thoughts/#{@thought.id}/"
-    else
-      redirect "/lists/#{@list.id}/thoughts/edit/#{@thought.id}/"
-    end
-  else
-    redirect "/lists/#{@list.id}/thoughts/"
-  end
-end
-
-delete '/lists/:listId/thoughts/:id/?' do
-  @list = List.get(params[:listId])
-  @thought = Thought.get(params[:id])
-  if @thought.list.id === @list.id
-    @thought.destroy
-  end
-  redirect "/lists/#{@list.id}/thoughts/"
+delete '/ideas/:id/?' do
+  @idea = Idea.get(params[:id])
+  @idea.destroy
+  redirect '/ideas/'
 end
