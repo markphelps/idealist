0a7c23857dfd0e4daea67697ea31ef4a29000d34
Mark Phelps <mark.aaron.phelps@gmail.com>
2012-01-08 21:30:14 -0500

    Changed some selectors

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
-$('#idea-textarea').live('keydown', (function(e) {
+$("#idea-textarea").live('keydown', function(e) {
   var code = (e.keyCode ? e.keyCode : e.which);
   // if enter is pressed, not shift + enter
   if(!e.shiftKey && code == 13) {
     $("#idea-form").submit();
     return false;
   }
-}));
+});
